ba6b622dd5a8eb940e895ae79ccd52428a1e8824
Devin J. Pohly <djpohly@gmail.com>
2014-01-20 17:46:12 -0500

    add basic M-flag following
    Uses RAsm->bits to track whether the M processor flag is in 16-bit or
    8-bit mode.  We would need something more flexible to implement X-flag
    following as well.

--- libr/asm/arch/snes/snesdis.c
+++ libr/asm/arch/snes/snesdis.c
@@ -7,29 +7,71 @@
 #include <string.h>
 #include "snes_op_table.h"
 
-static int snesDisass(RAsmOp *op, const ut8 *buf, ut64 len){
-	if (len<snes_op[buf[0]].len)
-		return 0;
-	switch (snes_op[buf[0]].len) {
+static int snesDisass(RAsm *a, RAsmOp *op, const ut8 *buf, ut64 len)
+{
+	int bits, n;
+
+	switch(snes_op[buf[0]].type)
+	{
 		case SNES_OP_8BIT:
+			if (len < 1)
+				return 0;
 			sprintf(op->buf_asm,"%s",snes_op[buf[0]].name);
-			break;
+			return 1;
 		case SNES_OP_16BIT:
-			sprintf(op->buf_asm,snes_op[buf[0]].name,buf[1]);
-			break;
-		case SNES_OP_24BIT:
-			if(*buf==0x44 || *buf==0x54){
-				sprintf (op->buf_asm, snes_op[buf[0]].name,
-					buf[1], buf[2]);
-			} else {
-				sprintf (op->buf_asm, snes_op[buf[0]].name,
-					buf[1]+0x100*buf[2]);
+			if (len < 2)
+				return 0;
+			if (buf[0]==0xe2) {
+				if (buf[1] & 0x20) {
+					// SEP M-flag = go to 8bit
+					r_asm_set_bits(a, 8);
+				}
+				// TODO: track X flag (0x10) too
+			} else if(buf[0]==0xc2) {
+				if (buf[1] & 0x20) {
+					// REP M-flag = go to 16bit
+					r_asm_set_bits(a, 16);
+				}
+				// TODO: track X flag (0x10) too
 			}
-			break;
+			sprintf(op->buf_asm,snes_op[buf[0]].name,buf[1]);
+			return 2;
+		case SNES_OP_24BIT:
+		case SNES_OP_24BIT_2ARG:
+			if (len < 3)
+				return 0;
+			//sprintf(op->buf_asm, snes_op[buf[0]].name,buf[1]+0x100*buf[2]);
+			sprintf(op->buf_asm, snes_op[buf[0]].name,buf[2],buf[1]);
+			return 3;
+		case SNES_OP_24BIT_M:
+			bits = (a->bits == 8) ? 8 : 16;
+
+			if (len < 1 + bits/8)
+				return 0;
+			n = sprintf(op->buf_asm, snes_op[buf[0]].name);
+			switch(bits)
+			{
+				case 8:
+					sprintf(op->buf_asm + n, "%02x", buf[1]);
+					return 2;
+				case 16:
+					sprintf(op->buf_asm + n, "%02x%02x", buf[2], buf[1]);
+					return 3;
+			}
+			return 1;
+		case SNES_OP_24BIT_X:
+			// Assume 16-bit X/Y for now
+			if (len < 3)
+				return 0;
+			n = sprintf(op->buf_asm, "%s", snes_op[buf[0]].name);
+			sprintf(op->buf_asm + n, "%02x%02x", buf[2], buf[1]);
+			return 3;
 		case SNES_OP_32BIT:
-			sprintf (op->buf_asm, snes_op[buf[0]].name,
-				buf[1]+0x100*buf[2]+0x10000*buf[3]);
-			break;
+			if (len < 4)
+				return 0;
+			sprintf (op->buf_asm, snes_op[buf[0]].name,buf[1]+0x100*buf[2]+0x10000*buf[3]);
+			return 4;
+		default:
+			return 0;
 	}
-	return snes_op[buf[0]].len;
 }
--- libr/asm/p/asm_snes.c
+++ libr/asm/p/asm_snes.c
@@ -10,7 +10,7 @@
 #include "../arch/snes/snesdis.c"
 
 static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
-	int dlen = snesDisass(op,buf,len);
+	int dlen = snesDisass(a,op,buf,len);
 	if(dlen<0) dlen=0;
 	op->size=dlen;
 	return dlen;
